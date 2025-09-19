632ee0b8bfebbf830b8d82b8509ff66f5360c771
Anastasiia Kulyk <kuliknatasha17@gmail.com>
2025-09-19 23:39:53 +0300

    Update and rename remove_d_from_sortArr.cs to remove_dubl_and_el_from_sortArr.cs

--- remove_dubl_and_el_from_sortArr.cs
+++ remove_dubl_and_el_from_sortArr.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+//26 and 27
+using System.Collections.Specialized;
 
 public class Solution
 {
@@ -16,13 +17,25 @@ public class Solution
         }
         return k;
     }
-} 
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val) { nums[k] = nums[i]; k++; }
+        }
+        return k;  
+
+    }
+
+}
 
 public class Program
 {
     public static void Main()
     {
         int[] nums = { 1, 1, 3, 3, 3, 4, 5, 6, 6 };
+        int[] nums2 = { 1, 1, 3, 3, 3, 4, 5, 6, 6 };
         Console.WriteLine("array:");
         foreach (int i in nums)
         {
@@ -31,14 +44,25 @@ public class Program
 
         Console.WriteLine();
         
-        Solution solution = new Solution();
-        int k = solution.RemoveDuplicates(nums);
+        Solution solution1 = new Solution();
+        int k = solution1.RemoveDuplicates(nums);
 
         Console.WriteLine("amount of unique elements: " + k);
-        Console.WriteLine("new array:");
+        Console.WriteLine("new unique array:");
         for (int i = 0; i < k; i++)
         {
             Console.Write(nums[i] + ", ");
         }
+        Console.WriteLine();
+
+        Solution solution2 = new Solution();
+        int val = 3;
+        int not_val = solution2.RemoveElement(nums2, val);
+        Console.WriteLine("new array without '3' :");
+        for (int i = 0; i < not_val; i++)
+        {
+            Console.Write(nums2[i] + ", ");
+        }
     }
 }
+
